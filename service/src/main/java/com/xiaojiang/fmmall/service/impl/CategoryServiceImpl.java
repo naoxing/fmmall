5292fc77ae1046f847614f6c84eaf894b635f299
naoxing <87369683+naoxing@users.noreply.github.com>
2023-05-06 23:37:39 +0800

    1.对首页轮播图采用redis缓存,保存一天 2.对商品分类查询采用redis缓存,保存一天

--- service/src/main/java/com/xiaojiang/fmmall/service/impl/CategoryServiceImpl.java
+++ service/src/main/java/com/xiaojiang/fmmall/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,21 @@
 package com.xiaojiang.fmmall.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xiaojiang.fmmall.entity.CategoryVO;
+import com.xiaojiang.fmmall.entity.IndexImg;
 import com.xiaojiang.fmmall.mapper.CategoryMapper;
 import com.xiaojiang.fmmall.service.CategoryService;
 import com.xiaojiang.fmmall.vo.ResStatus;
 import com.xiaojiang.fmmall.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author xiaojiang
@@ -17,10 +27,30 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public ResultVo listCategories() {
-        return new ResultVo(ResStatus.OK,"success",categoryMapper.selectAllCategories2(0));
+        List<CategoryVO> categoryVOS = null;
+        String cateGoriesJson = stringRedisTemplate.boundValueOps("cateGories").get();
+        try {
+            if(cateGoriesJson!=null){ //redis查询不为空
+                JavaType javaType = objectMapper.getTypeFactory().constructParametricType(ArrayList.class, CategoryVO.class);
+                categoryVOS = objectMapper.readValue(cateGoriesJson, javaType);
+                return new ResultVo(ResStatus.OK,"success",categoryVOS);
+            }else { //为空数据库查询
+                categoryVOS = categoryMapper.selectAllCategories2(0);
+                stringRedisTemplate.boundValueOps("cateGories").set(objectMapper.writeValueAsString(categoryVOS));
+                //设置过期时间为一天
+                stringRedisTemplate.boundValueOps("indexImgs").expire(1, TimeUnit.DAYS);
+                return new ResultVo(ResStatus.OK,"success",categoryVOS);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- service/src/main/java/com/xiaojiang/fmmall/service/impl/IndexImgServiceImpl.java
+++ service/src/main/java/com/xiaojiang/fmmall/service/impl/IndexImgServiceImpl.java
@@ -1,14 +1,20 @@
 package com.xiaojiang.fmmall.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xiaojiang.fmmall.entity.IndexImg;
 import com.xiaojiang.fmmall.mapper.IndexImgMapper;
 import com.xiaojiang.fmmall.service.IndexImgService;
 import com.xiaojiang.fmmall.vo.ResStatus;
 import com.xiaojiang.fmmall.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author xiaojiang
@@ -19,13 +25,32 @@ import java.util.List;
 public class IndexImgServiceImpl implements IndexImgService {
     @Autowired
     private IndexImgMapper indexImgMapper;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public ResultVo listIndexImgs() {
-        List<IndexImg> indexImgs = indexImgMapper.listIndexImgs();
-        if(indexImgs.size()==0){
-            return new ResultVo(ResStatus.NO,"fail",null);
+        List<IndexImg> indexImgs = null;
+        String imgsStr = stringRedisTemplate.boundValueOps("indexImgs").get();
+        try {
+            if(imgsStr!=null){
+                JavaType javaType = objectMapper.getTypeFactory().constructParametricType(ArrayList.class, IndexImg.class);
+                indexImgs = objectMapper.readValue(imgsStr, javaType);
+                return new ResultVo(ResStatus.OK,"success",indexImgs);
+            }else {
+                indexImgs = indexImgMapper.listIndexImgs();
+                if(indexImgs.size()==0){
+                    return new ResultVo(ResStatus.NO,"fail",null);
+                }
+                stringRedisTemplate.boundValueOps("indexImgs").set(objectMapper.writeValueAsString(indexImgs));
+                //设置过期时间为一天
+                stringRedisTemplate.boundValueOps("indexImgs").expire(1, TimeUnit.DAYS);
+                return new ResultVo(ResStatus.OK,"success",indexImgs);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
-        return new ResultVo(ResStatus.OK,"success",indexImgs);
     }
 }
